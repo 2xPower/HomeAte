5405045276ba2d39cb5da46b87914d23f1bf338e
xorcrud <simon@outlook.be>
2017-12-14 11:56:01 +0100

    added gitignore and improved knx connection test with state verification

--- Tests/TwicePower.Homeate.Binding.Knx.Testing/KnxBindingTests.cs
+++ Tests/TwicePower.Homeate.Binding.Knx.Testing/KnxBindingTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Twicepower.Homeate.Contracts;
 using TwicePower.Homeate.Binding.Knx;
 using Xunit;
 
@@ -10,15 +11,27 @@ namespace TwicePower.Homeate.Binding.Knx.Testing
     public class KnxBindingTests
     {
         [Fact]
-        public async Task A_Connection_Can_Be_Made_And_Disconnected()
+        public async Task A_Connection_Can_Be_Made_And_Disconnected_With_Correct_State_Events()
         {
             var binding = new KnxBinding();
+            var stateList = new List<BindingState>();
+            Assert.Equal(BindingState.Uninitialized, binding.State);
+            binding.BindingStateChanged += (sender, state) =>
+            {
+                stateList.Add(state);
+            };
             await binding.Init();
             Assert.True(binding.State == Twicepower.Homeate.Contracts.BindingState.Disconnected);
+            Assert.Equal(BindingState.Initializing, stateList[0]);
+            Assert.Equal(BindingState.Disconnected, stateList[1]);            
             await binding.Connect();
             Assert.True(binding.State == Twicepower.Homeate.Contracts.BindingState.Connected);
+            Assert.Equal(BindingState.Connecting, stateList[2]);
+            Assert.Equal(BindingState.Connected, stateList[3]);
             await binding.Disconnect();
             Assert.True(binding.State == Twicepower.Homeate.Contracts.BindingState.Disconnected);
+            Assert.Equal(BindingState.Disconnecting, stateList[4]);
+            Assert.Equal(BindingState.Disconnected, stateList[5]);
         }
 
         [Fact]
@@ -32,5 +45,7 @@ namespace TwicePower.Homeate.Binding.Knx.Testing
 
             await binding.Disconnect();
         }
+
+        
     }
 }
